bca912a47abd05133a537eaf258a069e88f02172
thunugurisanthosh <thunugurisanthosh@gmail.com>
2025-10-25 17:46:44 +0530

    hi

--- config.py
+++ config.py
@@ -8,10 +8,12 @@ BLOCK_DURATION = 3600            # Block duration in seconds (optional)
 EMAIL_ALERT = True                # Enable email notifications
 
 # Email settings
-SENDER_EMAIL = ""mongocelogo@gmail.com
-ADMIN_EMAIL = ""mongocelogo@gmail.com"
-SMTP_SERVER = "localhost"
-SMTP_PORT = 25
+SENDER_EMAIL = "thunugurisanthosh@gmail.com"       # From address (must match SMTP_USER)
+ADMIN_EMAIL = "thunugurisanthosh@gmail.com"        # Your inbox to receive alerts
+SMTP_SERVER = "smtp.gmail.com"              # Gmail SMTP server
+SMTP_PORT = 587                             # TLS port
+SMTP_USER = "thunugurisanthosh@gmail.com"          # Your Gmail login
+SMTP_PASS = "ffjm wvof nhtx kkcw" 
 
 # Log file for blocked IPs
 BLOCK_LOG_FILE = "blocked_ips.log"
