f3d8e205ab14ecf8ccd755c92971eb75164677c5
TUNUGURU SANTHOSH <51983284+thunugurisanthosh@users.noreply.github.com>
2025-10-25 17:58:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,12 @@ BLOCK_DURATION = 3600            # Block duration in seconds (optional)
 EMAIL_ALERT = True                # Enable email notifications
 
 # Email settings
-SENDER_EMAIL = "thunugurisanthosh@gmail.com"       # From address (must match SMTP_USER)
-ADMIN_EMAIL = "thunugurisanthosh@gmail.com"        # Your inbox to receive alerts
+SENDER_EMAIL = "your_email@gmail.com"       # From address (must match SMTP_USER)
+ADMIN_EMAIL = "your_email@gmail.com"        # Your inbox to receive alerts
 SMTP_SERVER = "smtp.gmail.com"              # Gmail SMTP server
 SMTP_PORT = 587                             # TLS port
-SMTP_USER = "thunugurisanthosh@gmail.com"          # Your Gmail login
-SMTP_PASS = "ffjm wvof nhtx kkcw" 
+SMTP_USER = "your_email@gmail.com"          # Your Gmail login
+SMTP_PASS = "your_app_password"
 
 # Log file for blocked IPs
 BLOCK_LOG_FILE = "blocked_ips.log"
