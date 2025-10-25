07821f4106d37c9d7543f3bac94d75448562d7e6
thunugurisanthosh <thunugurisanthosh@gmail.com>
2025-10-25 17:51:16 +0530

    hi

--- alert.py
+++ alert.py
@@ -1,8 +1,7 @@
-
 #!/usr/bin/env python3
 import smtplib
 from email.mime.text import MIMEText
-from config import SENDER_EMAIL, ADMIN_EMAIL, SMTP_SERVER, SMTP_PORT
+from config import SENDER_EMAIL, ADMIN_EMAIL, SMTP_SERVER, SMTP_PORT, SMTP_USER, SMTP_PASS
 
 def send_alert(ip, reason="Multiple failed login attempts"):
     try:
@@ -11,9 +10,16 @@ def send_alert(ip, reason="Multiple failed login attempts"):
         msg['From'] = SENDER_EMAIL
         msg['To'] = ADMIN_EMAIL
 
-        s = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-        s.send_message(msg)
-        s.quit()
+        # Connect to Gmail SMTP with TLS
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        server.ehlo()              # Identify yourself to the server
+        server.starttls()          # Start TLS encryption
+        server.ehlo()
+        server.login(SMTP_USER, SMTP_PASS)
+        server.send_message(msg)
+        server.quit()
+
         print(f"[ALERT] Email sent for blocked IP: {ip}")
+
     except Exception as e:
         print(f"[ERROR] Email alert failed: {e}")
