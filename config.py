58771b77e56e4292aff25c47b310c88225429965
thunugurisanthosh <thunugurisanthosh@gmail.com>
2025-10-25 17:20:47 +0530

    hi

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ BLOCK_DURATION = 3600            # Block duration in seconds (optional)
 EMAIL_ALERT = True                # Enable email notifications
 
 # Email settings
-SENDER_EMAIL = "admin@example.com"
-ADMIN_EMAIL = "your_email@example.com"
+SENDER_EMAIL = ""mongocelogo@gmail.com
+ADMIN_EMAIL = ""mongocelogo@gmail.com"
 SMTP_SERVER = "localhost"
 SMTP_PORT = 25
 
